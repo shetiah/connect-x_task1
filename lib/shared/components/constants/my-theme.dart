84a533fde6de2adfd12f714688ba2f591fc1eb29
Abdelrahman shetiah <abdrahamansheiah@gmail.com>
2024-01-29 03:34:39 +0200

    test

---
+++ lib/shared/components/constants/my-theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData myTheme=ThemeData(
+textTheme: const TextTheme(
+bodyMedium: TextStyle(
+  fontFamily: '',
+  fontSize:23 ,
+),
+headlineSmall: TextStyle(),
+headlineLarge: TextStyle(),
+)
+);
